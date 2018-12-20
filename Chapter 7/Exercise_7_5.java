62c863cc91d47111484145a48451e5b36d8d40e6
Zack Friedman <thiccabun@gmail.com>
2018-12-20 07:25:14 -0700

    Updated files
    Exercise 7_5 update

--- Chapter 7/Exercise_7_5.java
+++ Chapter 7/Exercise_7_5.java
@@ -1,12 +1,14 @@
 // Zack Friedman 12/18/18
 
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Exercise_7_5 {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		int[] numbers = new int[10];
 		int numberOfDistinctValues = 0;
+		ArrayList<Integer> array = new ArrayList<Integer>();
 		
 		System.out.print("Enter 10 numbers separated by a space: ");
 		for (int i = 0; i < numbers.length; i++) {
@@ -16,19 +18,21 @@ public class Exercise_7_5 {
 			for (int j = 0; j < numberOfDistinctValues; j++) {
 				if (value == numbers[j]) {
 					isInTheArray = true;
-				} // end of if statement
-			} // end of for loop
+				} // if end
+			} // for end
 			if (!isInTheArray) {
 				numbers[numberOfDistinctValues] = value;
 				numberOfDistinctValues++;
-			} // end of if statement
-		} // end of for loop
+			} // if end
+		} // for end
 		System.out.println("The number of distinct numbers is " + numberOfDistinctValues);
 		System.out.println("List without duplicates is: ");
 		for (int i = 0; i < numbers.length; i++) {
 			if (!numbers.equals(0)) {
-			System.out.print(numbers[i] + " ");
-			} // end of if statement
-		} // end of for loop
-	} // end of main method
+				array.add(numbers[i]);
+				array.remove(Integer.valueOf(0));
+			} // if end
+		} // for end
+		System.out.print(array + " ");
+	} // main end
 }
